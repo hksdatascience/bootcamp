16dd6e6e5989f15a9d7c50f3919bb25a8c987e63
Ryan Brellenthin <rbrellen@users.noreply.github.com>
2019-02-07 16:13:27 -0500

    Remove files that have been moved

--- week_01/read_write.R
+++
@@ -1,8 +0,0 @@
-# Read iris dataset and store as df
-df <- read.csv('iris.csv')
-
-# View a summary of all variables
-summary(df)
-
-# Write df object and save as iris2.csv
-write.csv(df, 'iris2,csv', row.names = FALSE)
